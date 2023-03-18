01927c774bde0e9c65a906e8416ecbd609feff36
spoorthi-987 <spoorthimandala@gmail.com>
2023-03-18 11:19:15 +0530

    asdm

--- one.java
+++ one.java
@@ -1,7 +1,7 @@
 class one {
     public static void main(String args[]) {
         int a = 5, b = 6;
-        int c = a + b;
+        int c = a - b;
         System.out.println("hi");
         System.out.println(c);
     }
